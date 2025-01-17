3e76af79598d861ef9a120c4f6ea29cabffb0ab5
Tushar D. S. <tushar.ds31@outlook.com>
2025-01-17 18:04:44 +0530

    Create p3.sh

---
+++ p3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#----------------------------------------------------start--------------------------------------------------#
+
+read -p "Enter ZONE: " ZONE
+
+gcloud sql instances create myinstance \
+  --root-password=quicklab \
+  --database-version=MYSQL_8_0 \
+  --tier=db-n1-standard-4 \
+  --region="${ZONE%-*}"
+
+gcloud sql databases create guestbook --instance=myinstance
+
+
+
+#-----------------------------------------------------end----------------------------------------------------------#
